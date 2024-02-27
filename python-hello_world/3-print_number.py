d2f29daf00b7df99fed4851be0240e812921ecd2
maabu025 <m.abu@alustudent.com>
2024-02-27 04:06:42 -0800

    Task 3

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 number = 98
-print(f"{number}, followed by Battery street,\n")
+print(f"{number}, followed by Battery street,\n)
+        print("positive")
+        print("negative")
+        print("zero")
+        print("wrong type")
