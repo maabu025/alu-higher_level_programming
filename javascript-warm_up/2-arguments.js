dd0ed1f9cabd4a47938325dd309490dec85bad74
maabu025 <m.abu@alustudent.com>
2024-05-28 00:53:46 -0700

    All fixed

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const array = process.argv.slice(2);
+if (array.length === 0) console.log('No argument');
+else if (array.length > 1) console.log('Arguments found');
+else if (array.length === 1) console.log('Argument found');
---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+const string = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+string.forEach((value, index) => console.log(string[index]));
